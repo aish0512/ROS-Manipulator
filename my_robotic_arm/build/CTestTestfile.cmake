3cd1810e37e58ef4ae7695df906b39cd7467c4c0
Aishwarya Palit <aishwarya.palit01@gmail.com>
2023-11-13 17:32:58 +0530

    effortntrajectorycontroller

---
+++ my_robotic_arm/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/aishwarya/Desktop/my_robotic_arm/src
+# Build directory: /home/aishwarya/Desktop/my_robotic_arm/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mrm_desp")
---
+++ my_robotic_arm/build/mrm_desp/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aishwarya/Desktop/my_robotic_arm/src/mrm_desp
+# Build directory: /home/aishwarya/Desktop/my_robotic_arm/build/mrm_desp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
